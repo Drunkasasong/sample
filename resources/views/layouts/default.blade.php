848974776e74f2aa14d26c0f7c77d9157faf43ee
Sauron <vagrant@homestead>
2018-07-19 13:45:10 +0000

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html>
-<head>
+  <head>
     <title>@yield('title', 'Sample App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="/css/app.css">
-</head>
-<body>
+  </head>
+  <body>
     @include('layouts._header')
-    
+
     <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
-    </div>    
-</body>
+      </div>
+    </div>
+  </body>
 </html>
