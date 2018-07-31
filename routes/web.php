feb7f4c5f0d171f098dec315663011c1594f7fac
Sauron <vagrant@homestead>
2018-07-31 03:00:41 +0000

    Add user following

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,9 @@ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
 // 微博相关
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+// 粉丝关注
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+// 取消关注
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
